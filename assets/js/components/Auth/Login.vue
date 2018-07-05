9820dbc753683a990ffb7132bbf2769bbbd3bf3d
Edward Lance Lorilla <edwardlorilla1986@gmail.com>
2018-07-06 00:14:42 +0800

    handling some errors

--- assets/js/components/Auth/Login.vue
+++ assets/js/components/Auth/Login.vue
@@ -1,50 +1,58 @@
 <template>
-    <transition  name="fade">
+    <transition name="fade">
         <div v-if="show" class="login-box">
-        <pre-loader/>
-        <div class="login-logo">
-            <a href="../../index2.html"><b>Admin</b>LTE</a>
-        </div>
-        <div class="card">
-            <div class="card-body login-card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
-
-                <form @submit.prevent="login">
-                    <div class="form-group has-feedback">
-                        <label for="email">Email address</label>
-                        <input id="email" v-model="credential.email" type="email" class="form-control" placeholder="Email">
-                    </div>
-                    <div class="form-group has-feedback">
-                        <label for="password">Password</label>
-                        <input id="password" v-model="credential.password" type="password" class="form-control" placeholder="Password">
-                    </div>
-                    <div class="row">
-                        <div class="col-8">
-                            <div class="checkbox icheck">
-                                <label>
-                                    <input type="checkbox"> Remember Me
-                                </label>
-                            </div>
-                        </div>
-                        <!-- /.col -->
-                        <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>
-                <!-- /.social-auth-links -->
-
-                <p class="mb-1">
-                    <a href="#">I forgot my password</a>
-                </p>
-                <p class="mb-0">
-                    <a href="register.html" class="text-center">Register a new membership</a>
-                </p>
+            <pre-loader/>
+            <div class="login-logo">
+                <a href="../../index2.html"><b>Admin</b>LTE</a>
             </div>
-            <!-- /.login-card-body -->
+            <div class="card">
+                <div class="card-body login-card-body">
+                    <p class="login-box-msg">Sign in to start your session</p>
+
+                    <form @submit.prevent="login">
+                        <div class="form-group has-feedback">
+                            <label for="email">Email address</label>
+                            <input id="email" v-model="credential.email" type="email" class="form-control"
+                                   :class="errors.email ? 'is-invalid' : ''" placeholder="Email">
+                            <span v-if="errors.email" v-for="error in errors.email" class="invalid-feedback" role="alert">
+                                        <strong>{{ error }}</strong>
+                                    </span>
+                        </div>
+                        <div class="form-group has-feedback">
+                            <label for="password">Password</label>
+                            <input id="password" v-model="credential.password" type="password"  :class="errors.password ? 'is-invalid' : ''"  class="form-control"
+                                   placeholder="Password">
+                            <span v-if="errors.password" v-for="error in errors.password" class="invalid-feedback" role="alert">
+                                        <strong>{{ error }}</strong>
+                                    </span>
+                        </div>
+                        <div class="row">
+                            <div class="col-8">
+                                <div class="checkbox icheck">
+                                    <label>
+                                        <input type="checkbox"> Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-4">
+                                <button type="submit" :disabled="isDisabled" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                    </form>
+                    <!-- /.social-auth-links -->
+
+                    <p class="mb-1">
+                        <a href="#">I forgot my password</a>
+                    </p>
+                    <p class="mb-0">
+                        <a href="register.html" class="text-center">Register a new membership</a>
+                    </p>
+                </div>
+                <!-- /.login-card-body -->
+            </div>
         </div>
-    </div>
     </transition>
 </template>
 <style>
@@ -52,10 +60,13 @@
     @import url('./../../../libs/admin-lte/adminlte.min.css');
     @import url('./../../../libs/iCheck/square/blue.css');
     @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700');
+
     .fade-enter-active, .fade-leave-active {
         transition: opacity .5s;
     }
-    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
+    {
         opacity: 0;
     }
 </style>
@@ -71,21 +82,29 @@
     })
     export default{
         data(){
-            return{
-                credential:{
+            return {
+                errors: {},
+                credential: {
                     email: '',
                     password: ''
                 },
-                show: true
+                show: true,
+                isDisabled: false
             }
         },
-        methods:{
+        methods: {
             login(){
                 let vm = this
-
+                vm.errors = ''
+                vm.isDisabled = true
                 axios.post('/login', vm.credential).then(function () {
                     window.location.href = window.location.href;
+
                     vm.show = false
+                }).catch(function (error) {
+                    vm.isDisabled = false
+                    console.log(error.response)
+                    vm.errors = error.response.data.errors;
                 })
             }
         }
---
+++ views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+    <!-- Styles -->
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    @yield('css')
+</head>
+<body class="hold-transition login-page sidebar-mini">
+@yield('content')
+@yield('js')
+
+</body>
+</html>
